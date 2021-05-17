8af13141163600904f1d8f1ecdc39b2da23150e8
Stanislas Polu <spolu@openai.com>
2021-05-17 09:16:44 +0200

    [clean-up] module-ize run_lean_and_save_results.py

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -17,9 +17,9 @@ lean --make src/empty.lean
 bash _target/deps/mathlib/scripts/mk_all.sh
 mkdir "$DATA_DIR"
 
-# python tools/run_lean_and_save_results.py _target/deps/mathlib/src/category_theory/currying.lean "$DATA_DIR"
-# python3 tools/run_lean_and_save_results.py src/empty.lean "$DATA_DIR"
-python3 tools/run_lean_and_save_results.py _target/deps/mathlib/src/all.lean "$DATA_DIR"
+# python3 -m lean_proof_recording.pipeline.run_lean_and_save_results.py _target/deps/mathlib/src/category_theory/currying.lean "$DATA_DIR"
+# python3 -m lean_proof_recording.pipeline.run_lean_and_save_results.py src/empty.lean "$DATA_DIR"
+python3 -m lean_proof_recording.pipeline.run_lean_and_save_results.py _target/deps/mathlib/src/all.lean "$DATA_DIR"
 
 python3 tools/extract_trace_data.py "$DATA_DIR"
 python3 tools/extract_proof_data.py "$DATA_DIR"
